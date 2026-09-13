4d14a1b6e01d41d15367c7a294d3073603c876e4
Qing-Novel <136884931+Qing-Novel@users.noreply.github.com>
2026-09-13 11:34:07 +0800

    Add enableBackgroundMessageService to SettingsMap

--- lib/shared/models/settings_map.dart
+++ lib/shared/models/settings_map.dart
@@ -52,6 +52,7 @@ class SettingsMap with SettingsMapMappable {
     required this.collapseAppBarWhenScroll,
     required this.threadFloorInteractionMode,
     required this.textScaleFactor,
+    required this.enableBackgroundMessageService,
     this.windowMaximized = false,
   });
 
@@ -100,6 +101,7 @@ class SettingsMap with SettingsMapMappable {
   final bool collapseAppBarWhenScroll;
   final ThreadFloorInteractionMode threadFloorInteractionMode;
   final double textScaleFactor;
+  final bool enableBackgroundMessageService;
 
   SettingsMap copyWithKey<T>(SettingsKeys<T> key, T? value) {
     assert(
@@ -156,6 +158,9 @@ class SettingsMap with SettingsMapMappable {
         threadFloorInteractionMode: value as ThreadFloorInteractionMode?,
       ),
       SettingsKeys.textScaleFactor => copyWith(textScaleFactor: value as double?),
+      SettingsKeys.enableBackgroundMessageService => copyWith(
+  enableBackgroundMessageService: value as bool?,
+),
     };
   }
 }
