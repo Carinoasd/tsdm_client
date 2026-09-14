fc0dead8336111276ab3322a7277dc9764c160c3
Qing-Novel <136884931+Qing-Novel@users.noreply.github.com>
2026-09-14 21:59:15 +0800

    第三十六步：修复作者提到的问题

--- lib/features/settings/repositories/settings_repository.dart
+++ lib/features/settings/repositories/settings_repository.dart
@@ -142,8 +142,6 @@ final class SettingsRepository with LoggerMixin {
       collapseAppBarWhenScroll: s.extract(_SK.collapseAppBarWhenScroll),
       threadFloorInteractionMode: s.extract(_SK.threadFloorInteractionMode),
       textScaleFactor: s.extract(_SK.textScaleFactor),
-      rememberExitChoice: s.extract(_SK.rememberExitChoice),
-      exitAction: s.extract(_SK.exitAction),
     );
   }
 
