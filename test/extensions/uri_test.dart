731c1ba6dc0db5f0b2f16cd355ea4147b911c07f
Qing-Novel <136884931+Qing-Novel@users.noreply.github.com>
2026-09-19 19:31:45 +0800

    修正回归测试

--- test/extensions/uri_test.dart
+++ test/extensions/uri_test.dart
@@ -3,19 +3,29 @@ import 'package:tsdm_client/extensions/uri.dart';
 
 void main() {
   group('isForumHost 域名支持回归测试', () {
-    test('支持 www.tsdm39.com', () {
+    test('支持 https://www.tsdm39.com', () {
       final uri = Uri.parse('https://www.tsdm39.com/forum.php?mod=viewthread&tid=123');
-      expect(isForumHost(uri), isTrue);
+      expect(uri.isForumHost, isTrue);
     });
 
-    test('支持不带 www 的 tsdm39.com', () {
+    test('支持不带 www 的 https://tsdm39.com', () {
       final uri = Uri.parse('https://tsdm39.com/forum.php?mod=viewthread&tid=123');
-      expect(isForumHost(uri), isTrue);
+      expect(uri.isForumHost, isTrue);
     });
 
-    test('拒绝不受支持的 www.tsdm39.net', () {
+    test('支持 http 协议的论坛域名', () {
+      final uri = Uri.parse('http://www.tsdm39.com/forum.php?mod=viewthread&tid=123');
+      expect(uri.isForumHost, isTrue);
+    });
+
+    test('拒绝不受支持的 https://www.tsdm39.net', () {
       final uri = Uri.parse('https://www.tsdm39.net/forum.php?mod=viewthread&tid=123');
-      expect(isForumHost(uri), isFalse);
+      expect(uri.isForumHost, isFalse);
+    });
+
+    test('拒绝其他外站域名', () {
+      final uri = Uri.parse('https://example.com/forum.php?mod=viewthread&tid=123');
+      expect(uri.isForumHost, isFalse);
     });
   });
 }
